1237d9379d66b2a4f9a7c0691eaadf85ff5b7005
Nitesh-mint <nitesh.raya058@gmail.com>
2023-01-03 08:14:19 +0545

    initial commit

---
+++ java/DiameterOfBinaryTree.java
@@ -0,0 +1,15 @@
+import javax.swing.tree.TreeNode;
+public class DiameterOfBinaryTree{
+    int max=0;
+    public static void main(String[] args) {
+        
+    }
+    private int maxDepth(TreeNode root){
+        if(root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        
+        max = Math.max(max,left + right);
+        return Math.max(left,right) + 1;
+    }
+}
